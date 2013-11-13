f8c35b45b26ab1c4493a4e2edfe47e094aad50a1
Till Arzner <arti1011@hs-karlsruhe.de>
2013-11-13 13:44:26 +0100

    Bestellposition Domain anpassung java ee7

--- shop/src/main/java/de/shop/bestellverwaltung/domain/Bestellposition.java
+++ shop/src/main/java/de/shop/bestellverwaltung/domain/Bestellposition.java
@@ -1,73 +1,81 @@
 package de.shop.bestellverwaltung.domain;
 
-import static de.shop.util.Constants.MIN_ID;
+import static de.shop.util.Constants.ERSTE_VERSION;
+import static de.shop.util.Constants.KEINE_ID;
 
 import java.io.Serializable;
 import java.lang.invoke.MethodHandles;
 import java.net.URI;
 
+import javax.persistence.Basic;
+import javax.persistence.Cacheable;
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.Index;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-import javax.persistence.NamedQuery;
-import javax.persistence.Entity;
-import javax.persistence.NamedQueries;
 import javax.persistence.PostPersist;
+import javax.persistence.PostUpdate;
 import javax.persistence.Table;
 import javax.persistence.Transient;
+import javax.persistence.Version;
 import javax.validation.constraints.Min;
-import javax.validation.constraints.NotNull;
+import javax.xml.bind.annotation.XmlTransient;
 
-import org.codehaus.jackson.annotate.JsonIgnore;
 import org.jboss.logging.Logger;
 
 import de.shop.artikelverwaltung.domain.Artikel;
 
 
+/**
+ * @author <a href="mailto:Juergen.Zimmermann@HS-Karlsruhe.de">J&uuml;rgen Zimmermann</a>
+ */
 @Entity
-@Table(name = "bestellposition")
-@NamedQueries({
-    @NamedQuery(name  = Bestellposition.FIND_LADENHUETER,
-   	            query = "SELECT a"
-   	            	    + " FROM   Artikel a"
-   	            	    + " WHERE  a NOT IN (SELECT bp.artikel FROM Bestellposition bp)")
-})
+@Table(indexes = { @Index(columnList = "bestellung_fk"), @Index(columnList = "artikel_fk") })
+@Cacheable
 public class Bestellposition implements Serializable {
-
-	private static final long serialVersionUID = 1618359231454653714L;
+	private static final long serialVersionUID = 1031749849939138054L;
 	private static final Logger LOGGER = Logger.getLogger(MethodHandles.lookup().lookupClass());
 	
-	private static final String PREFIX = "Bestellposition.";
-	public static final String FIND_LADENHUETER = PREFIX + "findLadenhueter";
+	//FIXME Ladenhüter konstante fehlt?!
 	private static final int ANZAHL_MIN = 1;
-	
+
 	@Id
 	@GeneratedValue
 	@Column(nullable = false, updatable = false)
-	@Min(value = MIN_ID, message = "{bestellverwaltung.bestellposition.positionId.min}", groups = IdGroup.class)
-	private Long positionId;
+	private Long id = KEINE_ID;
+
+	@Version
+	@Basic(optional = false)
+	private int version = ERSTE_VERSION;
 	
-	@ManyToOne(optional = false)
-    @JoinColumn(name = "artikel_fk", nullable = false)
-	@NotNull(message = "{bestellverwaltung.bestellposition.artikel.notNull}")
-	@JsonIgnore
+	@Column(nullable = false)
+	@Min(value = ANZAHL_MIN, message = "{bestellposition.anzahl.min}")
+	private short anzahl;
+	
+	@ManyToOne
+	@JoinColumn(name = "artikel_fk", nullable = false)
+	//NICHT @NotNull, weil beim Anlegen der Artikel bei REST durch seine URI uebergeben wird
+	//NICHT @Valid, weil der Artikel vor der Bestellposition existiert
+	@XmlTransient
 	private Artikel artikel;
 	
 	@Transient
 	private URI artikelUri;
 	
-	@Column(name = "anzahl", nullable = false)
-	@Min(value = ANZAHL_MIN, message = "{bestellverwaltung.bestellposition.anzahl.min}")
-	@NotNull(message = "{bestellverwaltung.bestellposition.anzahl.notNull}")
-	private Long anzahl;
-	
 	public Bestellposition() {
 		super();
 	}
 	
-	public Bestellposition(Artikel artikel, Long anzahl) {
+	public Bestellposition(Artikel artikel) {
+		super();
+		this.artikel = artikel;
+		this.anzahl = 1;
+	}
+	
+	public Bestellposition(Artikel artikel, short anzahl) {
 		super();
 		this.artikel = artikel;
 		this.anzahl = anzahl;
@@ -75,75 +83,96 @@ public class Bestellposition implements Serializable {
 	
 	@PostPersist
 	private void postPersist() {
-		LOGGER.debugf("Neue Bestellposition mit ID=%d", positionId);
+		LOGGER.debugf("Neue Bestellposition mit ID=%d", id);
+	}
+	
+	@PostUpdate
+	private void postUpdate() {
+		LOGGER.debugf("Bestellposition mit ID=%s aktualisiert: version=%d", id, version);
+	}
+	
+	public Long getId() {
+		return id;
 	}
 
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public int getVersion() {
+		return version;
+	}
+
+	public void setVersion(int version) {
+		this.version = version;
+	}
+
+	public short getAnzahl() {
+		return anzahl;
+	}
+	public void setAnzahl(short anzahl) {
+		this.anzahl = anzahl;
+	}
 	
-	public Long getPositionId() {
-		return positionId;
-	}
-	public void setPositionId(Long positionId) {
-		this.positionId = positionId;
-	}
 	public Artikel getArtikel() {
 		return artikel;
 	}
 	public void setArtikel(Artikel artikel) {
 		this.artikel = artikel;
 	}
+
 	public URI getArtikelUri() {
 		return artikelUri;
 	}
+	
 	public void setArtikelUri(URI artikelUri) {
 		this.artikelUri = artikelUri;
-	}
-	public Long getAnzahl() {
-		return anzahl;
-	}
-	public void setAnzahl(Long anzahl) {
-		this.anzahl = anzahl;
 	}
 
 	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
+		result = prime * result + anzahl;
 		result = prime * result + ((artikel == null) ? 0 : artikel.hashCode());
-		result = prime * result
-				+ ((positionId == null) ? 0 : positionId.hashCode());
 		return result;
 	}
 
 	@Override
 	public boolean equals(Object obj) {
-		if (this == obj)
+		if (this == obj) {
 			return true;
-		if (obj == null)
+		}
+		if (obj == null) {
 			return false;
-		if (getClass() != obj.getClass())
+		}
+		if (getClass() != obj.getClass()) {
 			return false;
+		}
 		final Bestellposition other = (Bestellposition) obj;
+		if (anzahl != other.anzahl) {
+			return false;
+		}
 		if (artikel == null) {
-			if (other.artikel != null)
+			if (other.artikel != null) {
 				return false;
+			}
 		}
-		else if (!artikel.equals(other.artikel))
+		else if (!artikel.equals(other.artikel)) {
 			return false;
-		if (positionId == null) {
-			if (other.positionId != null)
-				return false;
 		}
-		else if (!positionId.equals(other.positionId))
-			return false;
 		return true;
 	}
 
 	@Override
 	public String toString() {
-		return "Bestellposition [positionId=" + positionId + ", artikel="
-				+ artikel + ", artikelUri=" + artikelUri + ", anzahl=" + anzahl
-				+ "]";
+		if (artikel == null) {
+			return "Bestellposition [id=" + id + ", version=" + version
+			       + ", artikelUri=" + artikelUri + ", anzahl=" + anzahl + "]";
+		}
+
+		return "Bestellposition [id=" + id + ", version=" + version + ", artikel.id=" + artikel.getId()
+		       + ", anzahl=" + anzahl + "]";
 	}
-	
-	
+
 }
